6944f38e898a116345307561c78f5c8aa1ad474c
bada komora <badakomora06@gmail.com>
2023-12-19 17:07:57 +0300

    betbutton styles attributes

--- src/components/BetButton.tsx
+++ src/components/BetButton.tsx
@@ -6,16 +6,20 @@ const styles = ({
   width: "100%",
   height: "45px",
   marginTop: "35px",
-  "> button": {
+  "> button[data-betbutton='2']": {
     width: "100%",
     height: "100%",
-    '& [data-betbutton="2"]': {
-      background: '#00b801"',
-    },
-
-    '& [data-betbutton="1"]': {
-      background: '#00e701',
-    },
+    background: '#00b801',
+    borderRadius: "5px",
+    border: "none",
+    "&:hover": {
+      backgroundColor: "#1fff20",
+    }
+  },
+  "> button[data-betbutton='1']": {
+    width: "100%",
+    height: "100%",
+    background: '#00e701',
     borderRadius: "5px",
     border: "none",
     "&:hover": {
